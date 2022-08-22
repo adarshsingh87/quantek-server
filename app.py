398c5cd86e62f2210d5f744873dd290540278c06
Adarsh singh <adarshmanoj.05.as@gmail.com>
2022-08-22 22:36:30 +0530

    added cors

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ def get_dups():
     html = request.args.get('html')
     return jsonify(getDups(str(html)))
 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
